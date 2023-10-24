5a34db63e6a20c031ebcf1f7c97ed02da02692a3
JokerSA <asatishubham2004@gmail.com>
2023-10-25 00:50:37 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div className="bg-black min-h-screen flex justify-center items-center">
+      <div className="bg-blue-500 h-96 w-96">
+        <h1 className="text-5xl text-center">URL Shortner</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
